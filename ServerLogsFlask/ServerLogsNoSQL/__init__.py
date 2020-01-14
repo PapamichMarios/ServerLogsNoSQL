17e207fc34f257bbe168cbd9442d233ec46042a8
ChristosC <christospapadopoulos07@gmail.com>
2020-01-14 23:56:38 +0200

    Methods 1 and 5 working, pushed some code to run easily on pycharm etc

--- ServerLogsFlask/ServerLogsNoSQL/__init__.py
+++ ServerLogsFlask/ServerLogsNoSQL/__init__.py
@@ -16,3 +16,7 @@ def create_app(config_object='ServerLogsNoSQL.settings'):
     app.register_blueprint(insert)
     
     return app
+if __name__=='__main__':
+    app = create_app()
+    app.debug = True
+    app.run(host='0.0.0.0',port=5000)
--- ServerLogsFlask/ServerLogsNoSQL/methods.py
+++ ServerLogsFlask/ServerLogsNoSQL/methods.py
@@ -12,7 +12,36 @@ def index():
 
 @methods.route('/method1', methods=['GET'])
 def getMethod1():
-    return 'Joey'
+    dayFrom = request.args.get('from')
+    dayTo = request.args.get('to')
+
+    if not dayFrom or not dayTo:
+        return 'Your url is not valid'
+
+    logCollection = mongo.db.log
+    result = logCollection.aggregate([
+        {
+            '$match': {
+                'log_timestamp': {
+                    '$gte': datetime.strptime(dayFrom, '%Y-%m-%d %H:%M:%S'),
+                    '$lte': datetime.strptime(dayTo, '%Y-%m-%d %H:%M:%S')
+                }
+            }
+        }, {
+        '$group': {
+            '_id': '$type',
+            'total_logs': {
+                '$sum': 1
+            }
+        }
+    }, {
+        '$sort': {
+            'total_logs': -1
+        }
+    }
+    ])
+
+    return '<pre>' + dumps(result,indent=2) + '</pre>'
     
 @methods.route('/method2', methods=['GET'])
 def getMethod2():
@@ -58,7 +87,36 @@ def getMethod2():
 
 @methods.route('/method3', methods=['GET'])
 def getMethod3():
-    return 'Joey'
+    logCollection = mongo.db.log
+    result = logCollection.aggregate(
+        [
+            {
+                '$project': {
+                    'day': {
+                        '$dateToString': {
+                            'format': '%Y-%m-%d',
+                            'date': '$log_timestamp'
+                        }
+                    },
+                    'source_ip': '$source_ip',
+                    'type': '$type'
+                }
+            }, {
+            '$match': {
+                'day': '2008-11-18'
+            }
+        }, {
+            '$group': {
+                '_id': {'type':'$type','source_ip':'$source_ip'},
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }
+        ], allowDiskUse=True
+    )
+
+    return '<pre>' + dumps(result, indent=2) + '</pre>'
 
 @methods.route('/method4', methods=['GET'])
 def getMethod4():
@@ -99,7 +157,36 @@ def getMethod4():
 
 @methods.route('/method5', methods=['GET'])
 def getMethod5():
-    return 'Joey'
+    logCollection = mongo.db.log
+    result = logCollection.aggregate([
+    {
+        '$group': {
+            '_id': '$referer',
+            'count': {
+                '$sum': 1
+            },
+            'resources': {
+                '$addToSet': '$resource'
+            }
+        }
+    }, {
+        '$project': {
+            'referer': '$_id',
+            '_id': 0,
+            'size': {
+                '$size': '$resources'
+            }
+        }
+    }, {
+        '$match': {
+            'size': {
+                '$gt': 1
+            }
+        }
+    }
+])
+
+    return '<pre>' + dumps(result,indent=2) + '</pre>'
 
 @methods.route('/method6', methods=['GET'])
 def getMethod6():
@@ -160,12 +247,27 @@ def getMethod6():
 
 @methods.route('/method7', methods=['GET'])
 def getMethod7():
-    
-    return 'Joey'
+    logCollection = mongo.db.admin
+    result = logCollection.aggregate([
+        {
+            '$unwind': {
+                'path': '$upvotes'
+            }
+        }, {
+        '$lookup': {
+            'from': 'log',
+            'localField': 'upvotes',
+            'foreignField': '_id',
+            'as': 'string'
+        }
+    }
+    ])
+
+    return '<pre>' + dumps(result, indent=2) + '</pre>'
 
 @methods.route('/method8', methods=['GET'])
 def getMethod8():
-    
+
     logCollection = mongo.db.admin
     result = logCollection.aggregate([
         {
